9a24c70203234a7a4f02c998debe61c2e5390b8e
matheuslima99 <maths.soares99@gmail.com>
2022-02-01 16:05:10 -0300

    configuring routes

--- src/pages/Links/index.tsx
+++ src/pages/Links/index.tsx
@@ -1,4 +1,3 @@
-const Home = () => {
+export const Links = () => {
   return <div>Pagina links</div>;
 };
-export default Home;
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
-import Links from "./pages/Links";
+import { Links } from "./pages/Links";
 
 export const Router = () => {
   return (
