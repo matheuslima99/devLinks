a64ef6984ed200400d4a1e3be9822f6cbef3383b
matheuslima99 <maths.soares99@gmail.com>
2022-02-01 21:43:33 -0300

    added animation

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
@@ -15,6 +15,10 @@ export const LogoArea = styled.div`
   justify-content: center;
   align-items: center;
 
+  img {
+    width: 170px;
+  }
+
   h1 {
     font-size: 50px;
     color: #fff;
@@ -23,7 +27,7 @@ export const LogoArea = styled.div`
   span {
     color: #fff;
     font-size: 16px;
-    margin: 10px 0 20px 0;
+    margin: 10px 0 16px 0;
   }
 
   @media (max-width: 650px) {
@@ -41,15 +45,26 @@ export const LogoArea = styled.div`
   }
 
   @media (max-width: 450px) {
-    img{
+    img {
       width: 110px;
     }
   }
 `;
+
+const slideY = keyframes`
+  from{
+    transform: translateY(90px)
+  } 
+  to{
+    transform: translateY(0px)
+  }
+`
+
 export const InputArea = styled.div`
   max-width: 500px;
   width: 100%;
   margin: 15px 0;
+  animation: ${slideY} 2s;
 
   div {
     width: 100%;
@@ -86,9 +101,15 @@ export const InputArea = styled.div`
     border: none;
     border-radius: 6px;
     outline: none;
+    transition: transform 0.7s;
+
+    :hover {
+      transform: scale(1.05);
+    }
   }
 
   @media (max-width: 650px) {
     width: 90%;
   }
 `;
+
